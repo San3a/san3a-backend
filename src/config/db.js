00d83ed34b06958314bc7348bf168e890146cace
Terror-1 <elfarhosam487@gmail.com>
2025-11-11 14:38:36 +0200

    chore: update environment variables and server configuration to include SERVER_URL

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ import { MONGO_URL } from './config.js';
 const dbConnection = async () => {
     try {
         await mongoose.connect(MONGO_URL);
-        console.log('✅ Database connected successfully');
+        console.log('Database connected successfully');
     } catch (error) {
-        console.error('❌ Error while connecting to the database:', error);
+        console.error('Error while connecting to the database:', error);
         throw error;
     }
 };
