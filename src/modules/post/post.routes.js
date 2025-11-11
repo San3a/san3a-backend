3970cc848e72b1a9d770b24e28f4a7dfdda66860
yousefmobelal <ym8370302@gmail.com>
2025-11-11 14:21:53 +0200

    add validation to: - create post - update post - update post status - update post selected offer

--- src/modules/post/post.routes.js
+++ src/modules/post/post.routes.js
@@ -7,13 +7,19 @@ import {
     updatePostStatus,
     updateSelectedOffer,
 } from '#src/modules/post/post.controller.js';
+import {
+    validateCreatePost,
+    validateUpdatePost,
+    validateUpdatePostStatus,
+    validateUpdateSelectedOffer,
+} from '#src/modules/post/post.validator.js';
 import express from 'express';
 
 const router = express.Router();
 
-router.route('/').get(getAllPosts).post(createPost);
-router.route('/:id').get(getPost).patch(updatePost).delete(deletePost);
-router.patch('/:id/status', updatePostStatus);
-router.patch('/:id/select-offer', updateSelectedOffer);
+router.route('/').get(getAllPosts).post(validateCreatePost, createPost);
+router.route('/:id').get(getPost).patch(validateUpdatePost, updatePost).delete(deletePost);
+router.patch('/:id/status', validateUpdatePostStatus, updatePostStatus);
+router.patch('/:id/select-offer', validateUpdateSelectedOffer, updateSelectedOffer);
 
 export default router;
