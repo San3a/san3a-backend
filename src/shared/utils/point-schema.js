56cb50f635a6c33bfc43f94bf07a006f490cd7ad
HassanAli381 <alhassanali599@gmail.com>
2025-11-11 18:54:02 +0200

    feat: add point schema for geospatial data handling

---
+++ src/shared/utils/point-schema.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const pointSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: [true, 'Point type is required!']
+    },
+    coordinates: {
+        type: [Number], // [latitude, longitude]
+        required: [true, 'Coordinates is required!'],
+        validate: [(val) => val.length === 2, 'Coordinates length must = 2 (latitude, longitude)']
+    }
+})
+
+export default pointSchema;
