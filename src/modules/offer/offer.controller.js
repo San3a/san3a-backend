605b076eb771238b0112d6632868481f08cc7d59
yousefmobelal <ym8370302@gmail.com>
2025-11-12 11:14:31 +0200

    formatting offer.controller file

--- src/modules/offer/offer.controller.js
+++ src/modules/offer/offer.controller.js
@@ -5,5 +5,4 @@ export const createOffer = factory.createOne(Offer);
 export const getAllOffers = factory.getAll(Offer, null, { id: 'post' });
 export const getOffer = factory.getOne(Offer);
 export const updateOffer = factory.updateOne(Offer);
-
 export const deleteOffer = factory.deleteOne(Offer);
