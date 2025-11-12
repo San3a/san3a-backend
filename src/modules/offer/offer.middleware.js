82f7fdaf1bfe5b851c656fa99ecc588a8e9f1257
yousefmobelal <ym8370302@gmail.com>
2025-11-12 10:54:51 +0200

    - make logic of adding post and technician to body in middleware instead of controller - check if the technician is the owner of the offer he wants to update - check if post exists with a middleware in offer.routes

---
+++ src/modules/offer/offer.middleware.js
@@ -0,0 +1,19 @@
+import { asyncHandler } from '#src/shared/utils/async-handler.js';
+import { StatusCodes } from 'http-status-codes';
+import AppError from '#src/shared/utils/app-error.js';
+import Post from '../post/post.model.js';
+
+export const setOfferBody = (req, res, next) => {
+    req.body.post = req.params.id;
+    req.body.technician = req.user._id;
+    next();
+};
+
+export const isPostAvailable = asyncHandler(async (req, res, next) => {
+    const { postId } = req.params;
+    const post = await Post.exists({ _id: postId });
+    if (!post) {
+        return next(new AppError('No post found with this ID', StatusCodes.NOT_FOUND));
+    }
+    next();
+});
--- src/modules/offer/offer.routes.js
+++ src/modules/offer/offer.routes.js
@@ -14,18 +14,28 @@ import {
 } from '#src/modules/offer/offer.controller.js';
 import { validateCreateOffer, validateUpdateOffer } from '#src/modules/offer/offer.validator.js';
 import { isAuthorized } from '#src/shared/middlewares/authorization.middleware.js';
+import restrictToOwner from '#src/shared/middlewares/check-owner.middleware.js';
 import express from 'express';
+import { isPostAvailable, setOfferBody } from '#src/modules/offer/offer.middleware.js';
+import Offer from './offer.model.js';
 
 const router = express.Router({ mergeParams: true });
+
+router.use(isPostAvailable);
 
 router
     .route('/')
     .get(isAuthorized(GET_ALL_OFFERS), getAllOffers)
-    .post(isAuthorized(CREATE_OFFER), validateCreateOffer, createOffer);
+    .post(isAuthorized(CREATE_OFFER), validateCreateOffer, setOfferBody, createOffer);
 router
     .route('/:id')
     .get(isAuthorized(GET_OFFER), getOffer)
-    .patch(isAuthorized(UPDATE_OFFER), validateUpdateOffer, updateOffer)
+    .patch(
+        isAuthorized(UPDATE_OFFER),
+        restrictToOwner(Offer, 'technician', 'You are not allowed to update this offer'),
+        validateUpdateOffer,
+        updateOffer
+    )
     .delete(isAuthorized(DELETE_OFFER), deleteOffer);
 
 export default router;
