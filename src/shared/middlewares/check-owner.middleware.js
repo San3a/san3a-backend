5a6a155ec8652b1bdce59aec28e75c5179a8bd68
yousefmobelal <ym8370302@gmail.com>
2025-11-12 10:17:43 +0200

    implement restrictToOwner middleware with customizable owner field

--- src/shared/middlewares/check-owner.middleware.js
+++ src/shared/middlewares/check-owner.middleware.js
@@ -1,14 +1,14 @@
 import AppError from '#src/shared/utils/app-error.js';
 import { asyncHandler } from '#src/shared/utils/async-handler.js';
 
-const restrictToOwner = (Model, message) =>
+const restrictToOwner = (Model, ownerField = 'user', message = null) =>
     asyncHandler(async (req, res, next) => {
         const doc = await Model.findById(req.params.id);
         if (!doc) {
             return next(new AppError(`${Model.modelName} not found`, 404));
         }
 
-        if (!doc.user._id.equals(req.id)) {
+        if (!doc[ownerField]._id.equals(req.user.id)) {
             return next(new AppError(message || 'You are not allowed to perform this action', 403));
         }
 
